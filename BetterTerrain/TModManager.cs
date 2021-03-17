56e25b620eff2f22d8c0a1a325fdc179d5754429
74oshua <joshua.lowry02@gmail.com>
2021-03-17 13:42:37 -0700

    Fixed issues created after refactor

--- BetterTerrain/TModManager.cs
+++ BetterTerrain/TModManager.cs
@@ -1,74 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using UnityEngine;
 
 namespace BetterTerrain
 {
-    static class TModManager
-    {
-        public static void UpdateTMods()
-        {
-            // check for all unused tmods
-            List<TerrainModifier> remaining_tmods = new List<TerrainModifier>();
-            foreach (BetterTerrain.ZoneInfo zone in HMAPManager.zone_info.Values)
-            {
-                remaining_tmods.AddRange(zone.tmods.Except(zone.deletable_tmods));
-            }
+	internal static class TModManager
+	{
+		public static List<ZDO> zdos_to_save = new List<ZDO>();
 
-            tmods_to_remove = TerrainModifier.m_instances.Except(remaining_tmods.Distinct().ToList()).ToList();
-        }
+		public static List<TerrainModifier> tmods_to_remove = new List<TerrainModifier>();
 
-        public static void DeleteTMod(TerrainModifier modifier)
-        {
-            ZNetView znview = modifier.gameObject.GetComponent<ZNetView>();
-            if (znview && znview.GetZDO() != null)
-            {
-                // save TerrainModifier ZDO
-                bool has_copy = false;
-                foreach (ZDO zdo in zdos_to_save)
-                {
-                    if (zdo.m_uid == znview.GetZDO().m_uid)
-                    {
-                        has_copy = true;
-                        break;
-                    }
-                }
-                if (!has_copy)
-                {
-                    zdos_to_save.Add(znview.GetZDO().Clone());
-                }
+		public static List<TerrainModifier> already_removed = new List<TerrainModifier>();
 
-                // claim ownership of the TerrainModifier and destroy it
-                modifier.enabled = false;
-                znview.ClaimOwnership();
-                ZNetScene.instance.Destroy(modifier.gameObject);
-            }
-        }
+		public static void UpdateTMods()
+		{
+			foreach (BetterTerrain.ZoneInfo zone in HMAPManager.zone_info.Values)
+			{
+				tmods_to_remove.AddRange(zone.deletable_tmods);
+			}
+			tmods_to_remove = tmods_to_remove.Except(already_removed).ToList();
+			tmods_to_remove = tmods_to_remove.Distinct().ToList();
+		}
 
-        public static void DeleteTMods()
-        {
-            // if this heightmap's zone has been loaded, save it's info in zone_info
-            int destroyed = 0;
-            for (int i = 0; i < tmods_to_remove.Count; i++)
-            {
-                if (tmods_to_remove[i] != null)
-                {
-                    //UnityEngine.Debug.Log("Deleting tmod in zone: (" + zone.x + ", " + zone.y + ")");
-                    destroyed++;
-                    DeleteTMod(tmods_to_remove[i]);
-                }
-                tmods_to_remove.Remove(tmods_to_remove[i]);
-            }
-        }
+		public static void DeleteTMods()
+		{
+			int destroyed = 0;
+			for (int i = 0; i < tmods_to_remove.Count; i++)
+			{
+				if (tmods_to_remove[i] != null)
+				{
+					destroyed++;
+					DeleteTMod(tmods_to_remove[i]);
+				}
+				already_removed.Add(tmods_to_remove[i]);
+			}
+		}
 
-        public static void Reset()
-        {
-            tmods_to_remove.Clear();
-            tmods_to_remove.Clear();
-        }
+		public static void DeleteTMod(TerrainModifier modifier)
+		{
+			ZNetView znview = modifier.gameObject.GetComponent<ZNetView>();
+			if (!znview || znview.GetZDO() == null)
+			{
+				return;
+			}
+			bool has_copy = false;
+			foreach (ZDO item in zdos_to_save)
+			{
+				if (item.m_uid == znview.GetZDO().m_uid)
+				{
+					has_copy = true;
+					break;
+				}
+			}
+			if (!has_copy)
+			{
+				zdos_to_save.Add(znview.GetZDO().Clone());
+			}
+			modifier.enabled = false;
+			znview.ClaimOwnership();
+			if (!new string[3] { "mud_road(Clone)", "digg(Clone)", "path(Clone)" }.Contains(modifier.name))
+			{
+				Debug.LogWarning("Deleting " + modifier.name);
+			}
+			Debug.Log("Deleting " + modifier.name);
+			ZNetScene.instance.Destroy(modifier.gameObject);
+		}
 
-        public static List<ZDO> zdos_to_save = new List<ZDO>();
-        public static List<TerrainModifier> tmods_to_remove = new List<TerrainModifier>();
-    }
+		public static void Reset()
+		{
+			tmods_to_remove.Clear();
+			tmods_to_remove.Clear();
+		}
+	}
 }
